7ad79a6185da44e06d72b19948bc6e6b7b18ebaf
Adhitya Primandhika <67240@hq.bni.co.id>
2024-04-26 18:21:38 +0700

    connect to db with sqlalchemy

--- main.py
+++ main.py
@@ -1,180 +1,34 @@
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware
 from pydantic import BaseModel, Field
 from openai import OpenAI
+
+from schema import Merchant as SchemaMerchant
+from schema import Category as SchemaCategory
+from schema import Merchant
+from schema import Category
+from models import Merchant as ModelMerchant
+from models import Category as ModelCategory
+
 import os
 from dotenv import load_dotenv
 
-# load .env file
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import or_, and_
+
+import requests
+
+# Load .env file
 load_dotenv()
 
+# Set up OpenAI client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# data mockup
-mock_data_merchants = [
-    {
-        "id": 1,
-        "name": "Amuya Cafe",
-        "sub_name": "Amuya Cafe Kemayoran",
-        "merchant_code": "AMYC",
-        "category_id": 10,
-        "logo": "test-amuya.jpg",
-        "website": "https://www.amuyacoffee.com/",
-        "latitude": -6.1549693,
-        "longitude": 106.8403106,
-        "address": "Gedung Graha Kana, Jl. Angkasa 1 No.Kav.4 Blok B16, Gn. Sahari Sel., Kota Jakarta Pusat",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"        
-    },
-    {
-        "id": 2,
-        "name": "Family Mart",
-        "sub_name": "Family Mart Pejompongan",
-        "merchant_code": "FAMI",
-        "category_id": 10,
-        "logo": "test-fami.jpg",
-        "website": "https://www.familymart.co.id/",
-        "latitude": -6.20369,
-        "longitude": 106.80206,
-        "address": "Menara BNI Pejompongan",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"        
-    }
-]
+# Set up sqlalchemy
+engine = create_engine(os.getenv("DB_URL"))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-mock_data_categories = [
-    {
-        "id": 1,
-        "name": "Uang Keluar",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 2,
-        "name": "Tabungan & Investasi",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 3,
-        "name": "Pinjaman",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 4,
-        "name": "Tagihan",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 5,
-        "name": "Hadiah & Amal",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 6,
-        "name": "Transportasi",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 7,
-        "name": "Belanja",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 8,
-        "name": "Top Up",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 9,
-        "name": "Hiburan",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 10,
-        "name": "Makanan & Minuman",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 11,
-        "name": "Biaya & Lainnya",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 12,
-        "name": "Hobi & Gaya Hidup",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 13,
-        "name": "Perawatan Diri",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 14,
-        "name": "Kesehatan",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 15,
-        "name": "Pendidikan",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 16,
-        "name": "Uang Masuk",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 17,
-        "name": "Gaji",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 18,
-        "name": "Pencairan Investasi",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 19,
-        "name": "Bunga",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 20,
-        "name": "Refund",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 21,
-        "name": "Pencairan Pinjaman",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-    {
-        "id": 22,
-        "name": "Cashback",
-        "created_at": "2023-11-15 01:02:03",
-        "updated_at": "2023-11-15 01:02:03"   
-    },
-]
 """
 trx_body = {
     "description": "FAMI FAMILY MART PEJOMPONGAN",
@@ -189,34 +43,65 @@ trx_body = {
 }
 """
 
-# function to retrieve data merchant
-def get_data_merchant(merchant_name, category):
-    cat_id = None
-    merchant = None
-    for cat in mock_data_categories:
-        if cat["name"].lower() == category.lower():
-            cat_id = cat["id"]
-            break
-    for merc in mock_data_merchants:
-        if merc["category_id"] == cat_id and merc["sub_name"].lower() == merchant_name.lower():
-            merchant = merc
-            break
-    
-    return merchant
+def get_data_google(query):
+    data = {
+        "textQuery": query
+    }
 
-# function to retrieve data category
-def get_category(Transaction):
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": os.getenv("GOOGLE_PLACE_API_KEY")
+    }
+
+    response = requests.post(os.getenv("BASE_URL"), json=data, headers=headers)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        print(data)
+        return data['results'][0]
+    else:
+        # Print an error message if the request failed
+        return None
+
+# Function to retrieve data merchant
+def get_data_merchant(merchant_name):
+    db = SessionLocal()
+    try:
+        # Execute the query
+        merchant = db.query(ModelMerchant).filter(or_(
+                str(ModelMerchant.sub_name).lower() == merchant_name.lower(),
+                ModelMerchant.sub_name.ilike(f"%{merchant_name}%")
+            )).first()
+        if merchant != None:
+            # Return the result
+            return merchant
+        else:
+            return None
+    finally:
+        # Close the session
+        db.close()
+
+# Function to retrieve data category
+def get_category(detail):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "Kamu adalah personal financial manager"},
-            {"role": "user", "content": "Kategorisasikan data transaksi berikut " + str(Transaction) + " dengan kategori ['Uang Keluar','Tabungan & Investasi', 'Pinjaman', 'Tagihan', 'Hadiah & Amal', 'Transportasi', 'Belanja', 'Top Up', 'Hiburan', 'Makanan & Minuman', 'Biaya & Lainnya', 'Hobi & Gaya Hidup', 'Perawatan Diri', 'Kesehatan', 'Pendidikan', 'Uang Masuk', 'Gaji', 'Pencairan Investasi', 'Bunga', 'Refund', 'Pencairan Pinjaman', 'Cashback'] jawab hanya dengan kategorinya saja. Jika input data transaksi terlalu random atau tidak dapat dimengerti atau tidak memiliki arti, masukkan ke dalam kateogri 'Other'"}
+            {"role": "user", "content": "Kategorisasikan data transaksi berikut " + str(detail) + " dengan kategori ['Uang Keluar','Tabungan & Investasi', 'Pinjaman', 'Tagihan', 'Hadiah & Amal', 'Transportasi', 'Belanja', 'Top Up', 'Hiburan', 'Makanan & Minuman', 'Biaya & Lainnya', 'Hobi & Gaya Hidup', 'Perawatan Diri', 'Kesehatan', 'Pendidikan', 'Uang Masuk', 'Gaji', 'Pencairan Investasi', 'Bunga', 'Refund', 'Pencairan Pinjaman', 'Cashback'] jawab hanya dengan kategorinya saja. Jika input data transaksi terlalu random atau tidak dapat dimengerti atau tidak memiliki arti, masukkan ke dalam kateogri 'Other'"}
         ]
     )
-    return completion.choices[0].message
+    result = {}
+    result["transaction_detail"] = detail
+    result["category"] = vars(completion.choices[0].message)["content"]
+    return result
 
 app = FastAPI()
 
+# to avoid csrftokenError
+app.add_middleware(DBSessionMiddleware, db_url=os.getenv("DB_URL"))
+
+# Model for transaction
 class Transaction(BaseModel):
     description: str
     entry_type: str
@@ -228,20 +113,36 @@ class Transaction(BaseModel):
     account_holder_id: str
     account_holder_type: str
 
-class Merchant(BaseModel):
+# Model for merchant
+class DataMerchant(BaseModel):
     name: str
-    category: str
 
 @app.get("/")
 async def home():
     return {"messages": "Welcome to API services"}
 
-# retrieve data merchant
-@app.post("/api/data-merchant")
-async def data_merchant(m: Merchant):
-    return get_data_merchant(m.name, m.category)
+# Retrieve all data from merchants table
+@app.get("/api/merchants")
+async def get_merchants():
+    db = SessionLocal()
+    merchants = db.query(ModelMerchant).all()
+    db.close()
+    return {"merchants": merchants}
 
-# categorize with LLM
+# Retrieve all data from categories table
+@app.get("/api/categories")
+async def get_categories():
+    db = SessionLocal()
+    categories = db.query(ModelCategory).all()
+    db.close()
+    return {"categories": categories}
+
+# Retrieve data merchant
+@app.post("/api/get-data-merchant")
+async def data_merchant(m: DataMerchant):
+    return get_data_merchant(m.name)
+
+# Categorize with LLM
 @app.post("/api/categorize-transaction")
 async def categorize_transaction(t: Transaction):
     return get_category(t)
---
+++ schema.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class Category(BaseModel):
+    id: int
+    name: str
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+    class Config:
+        from_attributes = True
+
+class Merchant(BaseModel):
+    id: int
+    name: str
+    sub_name: str
+    merchant_code: str
+    category_id: int
+    logo: str
+    website: str
+    latitude: float
+    longitude: float
+    address: str
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+    class Config:
+        from_attributes = True
